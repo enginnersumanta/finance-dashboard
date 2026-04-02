014b4763da44dfc748a4bdd7d62929a8db59a119
Sumanta Mandal <sumantamandal728@gmail.com>
2026-04-02 12:58:59 +0530

    updata

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Finance/' : '/',
+export default defineConfig({
+  base: '/finance-dashboard/',
   plugins: [react(), tailwindcss()],
-  base: "/finance-dashboard/",
-}))
+})
 
